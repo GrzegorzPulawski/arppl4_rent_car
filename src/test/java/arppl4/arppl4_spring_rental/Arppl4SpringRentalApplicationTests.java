1d28cddc0afb834ab45fe8e9d79d8acf58e46316
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2022-07-07 17:53:26 +0200

    inicjacja commit

---
+++ src/test/java/arppl4/arppl4_spring_rental/Arppl4SpringRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package arppl4.arppl4_spring_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arppl4SpringRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
